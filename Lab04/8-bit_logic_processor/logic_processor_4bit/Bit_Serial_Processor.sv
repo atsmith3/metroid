a3fd2943d3686cf794aedfb0d39756b6f752248c
--global <zgleason94@gmail.com>
2016-09-11 15:28:48 -0500

    Working on 8-bit logic processor

---
+++ Lab04/8-bit_logic_processor/logic_processor_4bit/Bit_Serial_Processor.sv
@@ -0,0 +1,8 @@
+module Bit_Serial_Processor(input [1:0] R,
+									 input Execute, LoadA, LoadB, Reset, Clk,
+									 input [2:0] F,
+									 output [7:0] Aval, Bval,
+									 output [6:0] AhexU, AhexL, BhexU, BhexL,
+									 output [3:0] LED);
+				testbench_8	test0();
+endmodule
