f65de5dc09b0f49da4663555a6b8ac1d39b50538
Andrew Smith <atsmith3@dcl-l416-18.ews.illinois.edu>
2016-09-11 19:13:41 -0500

    Ready to upload to board

--- Lab04/8-bit_logic_processor/logic_processor_4bit/Bit_Serial_Processor.sv
+++ Lab04/8-bit_logic_processor/logic_processor_4bit/Bit_Serial_Processor.sv
@@ -1,8 +1,21 @@
-module Bit_Serial_Processor(input [1:0] R,
-									 input Execute, LoadA, LoadB, Reset, Clk,
-									 input [2:0] F,
-									 output [7:0] Aval, Bval,
-									 output [6:0] AhexU, AhexL, BhexU, BhexL,
-									 output [3:0] LED);
-				testbench_8	test0();
+module Bit_Serial_Processor(
+						input logic   Clk,     // Internal
+                                Reset,   // Push button 0
+                                LoadA,   // Push button 1
+                                LoadB,   // Push button 2
+                                Execute, // Push button 3
+                  input  logic [7:0]  Din,     // input data
+                  input  logic [2:0]  F,       // Function select
+                  input  logic [1:0]  R,       // Routing select
+                  output logic [3:0]  LED,     // DEBUG
+                  output logic [7:0]  Aval,    // DEBUG
+                                Bval,    // DEBUG
+                  output logic [6:0]  AhexL,
+                                AhexU,
+                                BhexL,
+                                BhexU);
+
+										  
+	  Processor main(.*);
+		  
 endmodule
