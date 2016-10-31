079aebce35084792eeb30c384ef571688006f0b7
atsmith3 <atsmith3@illinois.edu>
2016-10-31 11:58:04 -0500

    New Lab 9

---
+++ Lab09_1/software/lab9_aes/aes.c
@@ -0,0 +1,14 @@
+/******************************************
+** Advanced Encryption Standard
+** Author: B-Con (b-con@b-con.us)
+** Copyright/Restrictions: GNU GPL
+** Disclaimer: This code is presented "as is" without any garuentees; said author holds
+               liability for no problems rendered by the use of this code.
+** Details: This code is the implementation of the AES algorithm, as specified by the
+            NIST in in publication FIPS PUB 197, availible on the NIST website at
+            http://csrc.nist.gov/publications/fips/fips197/fips-197.pdf .
+******************************************/
+
+#include <stdio.h>
+
+
